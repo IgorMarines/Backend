f51e6129acebb9ddc85456d403fbdf5452ba04e3
IgorMarines <igor.marines25@gmail.com>
2023-12-23 07:24:23 -0300

    fix: api

--- src/index.js
+++ src/index.js
@@ -1,96 +1,92 @@
-const Fastify = require("fastify");
-const neon = require("neon-js");
-const pg = require("pg");
-require("dotenv").config();
+const fastify = require("fastify")();
 
-const fastify = Fastify({
-  logger: false,
-});
+require("dotenv").config();
 
 fastify.register(require("@fastify/cors"), {
   origin: true,
 });
 
-const connectionString = process.env.DATABASE_URL;
-
-const pool = new pg.Pool({
-  connectionString: connectionString,
-});
-
-fastify.decorate("pg", {
-  query: async function (text, values) {
-    const client = await pool.connect();
-    try {
-      const result = await client.query(text, values);
-      return result.rows;
-    } finally {
-      client.release();
-    }
-  },
+fastify.register(require("@fastify/postgres"), {
+  connectionString:
+    "postgres://default:MFStQxT47sfa@ep-quiet-field-46801094-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require",
 });
 
 fastify.get("/teste/:numero", function (request, reply) {
   reply.send(`${request.params.numero}`);
 });
 
-fastify.get("/products", async function (request, reply) {
+fastify.get("/users", async function (request, reply) {
   try {
-    const result = await fastify.pg.query("SELECT * FROM produtos");
-    reply.send(result);
+    const result = await fastify.pg.query("SELECT * FROM usuarios");
+    reply.send(result.rows);
   } catch (error) {
-    reply.send(error);
+    console.error("Error fetching users:", error);
+    reply.status(500).send({
+      error: "Internal Server Error",
+      message: error.message, // Adicionando a mensagem de erro para maior detalhamento
+    });
   }
 });
 
-fastify.get("/products/:id", async function (request, reply) {
+fastify.get("/users/:id", async function (request, reply) {
   try {
+    const userId = Number(request.params.id);
     const result = await fastify.pg.query(
-      "SELECT * FROM produtos WHERE id = $1",
-      [Number(request.params.id)]
+      "SELECT * FROM usuarios WHERE id = $1",
+      [userId]
     );
-    reply.send(result);
+
+    if (result.rows.length === 0) {
+      reply.status(404).send("User not found");
+    } else {
+      reply.send(result.rows[0]);
+    }
   } catch (error) {
-    reply.send(error);
+    console.error("Error fetching user by id:", error);
+    reply.status(500).send("Internal Server Error");
   }
 });
 
-fastify.post("/products", async function (request, reply) {
+fastify.post("/users", async function (request, reply) {
   try {
     const result = await fastify.pg.query(
-      "INSERT INTO produtos (id, name, price) VALUES ($1, $2, $3)",
+      "INSERT INTO usuarios (id, name, price) VALUES ($1, $2, $3)",
       [request.body.id, request.body.name, request.body.price]
     );
     reply.send(result);
   } catch (error) {
-    reply.send(error);
+    console.error("Error creating user:", error);
+    reply.status(500).send("Internal Server Error");
   }
 });
 
-fastify.put("/products/:id", async function (request, reply) {
+fastify.put("/users/:id", async function (request, reply) {
   try {
     const result = await fastify.pg.query(
-      "UPDATE produtos SET name = $1, price = $2 WHERE id = $3",
+      "UPDATE usuarios SET name = $1, price = $2 WHERE id = $3",
       [request.body.name, request.body.price, Number(request.params.id)]
     );
     reply.send(result);
   } catch (error) {
-    reply.send(error);
+    console.error("Error updating user:", error);
+    reply.status(500).send("Internal Server Error");
   }
 });
 
-fastify.delete("/products/:id", async function (request, reply) {
+fastify.delete("/users/:id", async function (request, reply) {
   try {
     const result = await fastify.pg.query(
-      "DELETE FROM produtos WHERE id = $1",
+      "DELETE FROM usuarios WHERE id = $1",
       [Number(request.params.id)]
     );
     reply.send(result);
   } catch (error) {
-    reply.send(error);
+    console.error("Error deleting user:", error);
+    reply.status(500).send("Internal Server Error");
   }
 });
 
-fastify.listen({ port: 3000 }, function (error, address) {
+fastify.listen({ port: 3000 }, (error, address) => {
   if (error) {
     console.error(error);
     process.exit(1);
